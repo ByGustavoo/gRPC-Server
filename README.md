ec2aad14fef0182545e58fcef15555b51ba36d8c
Gustavo <132701636+ByGustavoo@users.noreply.github.com>
2026-08-06 21:47:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<div align="center"> <br> 
+  <img align="center" alt="guru-java" height="150" width="150" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg" />
+</div> 
+
+<br> 
+
+<div align="center">
+  Template de projeto Spring Boot desenvolvido para facilitar a criação de novas aplicações, fornecendo uma base sólida, padronizada e reutilizável para futuros desenvolvimentos.
+</div> 
+
+ <br> 
+
+## 🚀 Ferramentas Utilizadas
+
+* ☕️ Java 25
+
+* 🟢 Spring Boot 4.0.3
+  
+<br> 
+ 
+## 🖥️ Desenvolvedor
+
+### 🔵 LinkedIn: [Gustavo Correa](https://www.linkedin.com/in/gustavo-chauar-correa-946168269/)
---
+++ build.gradle.kts
@@ -0,0 +1,96 @@
+plugins {
+    java
+    id("jacoco")
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+version = "1.0.0"
+group = "br.com.software"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
+
+configurations.configureEach {
+    exclude(group = "ch.qos.logback", module = "logback-classic")
+    exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+}
+
+dependencies {
+
+    // Spring Boot
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // MapStruct
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+    // PostgreSQL
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.flywaydb:flyway-database-postgresql")
+
+    // Logging
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+
+    // Tests
+    testImplementation("org.mockito:mockito-core:5.12.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+    dependsOn(tasks.test)
+
+    reports {
+        html.required.set(true)
+    }
+
+    classDirectories.setFrom(
+        files(
+            classDirectories.files.map {
+                fileTree(it) {
+                    exclude(
+                        "**/config/**",
+                        "**/SpringBootTemplateApplication.class"
+                    )
+                }
+            }
+        )
+    )
+}
